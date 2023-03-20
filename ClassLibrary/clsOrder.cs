221631bde9eb11e937ca82d0aa306ffac0ee6b3d
P2672211 <P2672211@my365.dmu.ac.uk>
2023-03-20 15:18:59 +0000

    work

---
+++ ClassLibrary/clsOrder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class clsOrder
+
+
+    {
+
+        public int OrderID { get; set; }
+        public int CustomerID { get; set; }
+        public string ShippingAddress { get; set; }
+        public int OrderDate { get; set; }
+        public Object totalCost { get; set; }
+        public string PaymentMethod { get; set; }
+        
+
+    }
+}
